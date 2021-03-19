114a98267ed822ea98a7dc229159c223437e177e
Ravi reddy <reddyraviteja74@gmail.com>
2021-03-19 20:12:27 +0530

    footer added

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,13 +1,3 @@
 export default function Contact() {
-  return (
-    <div className="min-h-screen bg-green-200 bg-opacity-70">
-      <div className="align-middle">
-        <hr></hr>
-        Contact component starts...!
-        <p>This is contact</p>
-        Contact component ends..!
-        <hr></hr>
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function index() {
-  return <div className={`bg-red-300 min-h-screen `}>This is test</div>;
+  return (
+    <div className={`min-h-screen `}>
+      <div></div>
+      <div></div>
+    </div>
+  );
 }
