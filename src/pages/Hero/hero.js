164810adeed719b22aa613ce05bd52d95877600e
Ravi reddy <reddyraviteja74@gmail.com>
2021-03-21 15:12:48 +0530

    hero added

--- src/pages/Hero/hero.js
+++ src/pages/Hero/hero.js
@@ -2,10 +2,24 @@ import React from "react";
 
 export default function hero() {
   return (
-    <div className="  flex flex-row">
-      <div className="bg-yellow-500">Hi there..!</div>
-      <div className="bg-gradient-to-br from-green-400 to-cyan-500">
-        i am here..!
+    <div className="  flex flex-row border-b-4 font-sans font-bold text-3xl align-middle">
+      <div className="w-2/3  h-1/2 flex flex-col items-center justify-center justify-items-center mt-52">
+        <div>
+          Hello! I'm
+          <span className=" font-extrabold text-yellow-600"> Ravi reddy</span>
+        </div>
+        <div>I'm a Full-stack developer</div>
+        <div className=" flex flex-row mt-7  text-base">
+          <button className=" px-5 py-3  border-indigo-900 border-2 mx-5 hover:bg-black hover:text-green-50">
+            View more
+          </button>
+          <button className=" px-5 py-3  border-yellow-600 border-2 hover:bg-black hover:text-green-50  hover:border-opacity-0  mx-5">
+            Resume
+          </button>
+        </div>
+      </div>
+      <div className=" bg-yellow-400 w-1/3  h-1/2 items-center">
+        <img src="DP1.png" alt="pic" className=" w-10/12 h-1/2 "></img>
       </div>
     </div>
   );
--- src/pages/utils/Footer.js
+++ src/pages/utils/Footer.js
@@ -3,17 +3,21 @@ import React from "react";
 export default function footer() {
   return (
     <div>
-      <div className="flex flex-row min-h-screen bg-black bg-opacity-80  items-center justify-center">
+      <div className="flex flex-row min-h-screen items-center justify-center bg-gray-500">
         <div className="flex flex-col align-middle items-center justify-center mr-96">
           <span className=" text-yellow-50 text-2xl mt-10">Contact Me</span>
-          <form className="flex flex-col md:items-center justify-center">
+          <form className="flex flex-col md:items-center justify-center bg-yellow-300 p-10  rounded-xl">
             <input
               type="email"
               placeholder="Email"
-              className=" p-3 px-10 m-5"
+              className=" p-3 px-10 m-5 rounded"
             ></input>
-            <input type="text" placeholder="Message" className="p-10"></input>
-            <button className=" mt-5 p-3 px-8 bg-white text-center hover:bg-yellow-400">
+            <input
+              type="text"
+              placeholder="Message"
+              className="p-10 rounded"
+            ></input>
+            <button className=" mt-5 p-3 px-8 bg-white text-center hover:bg-green-400  rounded-xl">
               Send
             </button>
           </form>
